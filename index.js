912c853994554bdb64e29180c1c8c60c637f1b69
utsho <utsho111099@gmail.com>
2022-05-03 09:50:39 +0600

    server ready

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
@@ -7,10 +8,33 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@bookcluster.jjxng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("bookHouse").collection("users");
+        app.get('/test', async (req, res) => {
+            const query = {};
+            // const query = { name: 'utsho' };
+
+            const cursor = userCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send('Hey uthso!! server running...');
-})
+    res.send('Hey uthso!! your server running...');
+});
 
 app.listen(port, () => {
     console.log('Your server is runnning on: ', port);
-})
+});
